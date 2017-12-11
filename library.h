4e468cbd8178f64123b6446a4d437b5e265578c9
John Wass <wassj@ctc.com>
2017-12-11 17:30:12 -0500

    initial import

---
+++ library.h
@@ -0,0 +1,44 @@
+#ifndef EXAMPLE_PDAL_PLUGIN_LIBRARY_H
+#define EXAMPLE_PDAL_PLUGIN_LIBRARY_H
+
+// MyFilter.hpp
+
+#pragma once
+
+#include <pdal/Filter.hpp>
+#include <pdal/Stage.hpp>
+
+#include <memory>
+
+namespace pdal
+{
+
+    class Options;
+    class PointLayout;
+    class PointView;
+
+    class PDAL_DLL MyFilter : public Filter
+    {
+    public:
+        MyFilter() : Filter()
+        {}
+
+        static void * create();
+        static int32_t destroy(void *);
+        std::string getName() const;
+
+    private:
+        double m_value;
+        Dimension::Id m_myDimension;
+
+        virtual void addDimensions(PointLayoutPtr layout);
+        virtual void addArgs(ProgramArgs& args);
+        virtual PointViewSet run(PointViewPtr view);
+
+        MyFilter& operator=(const MyFilter&); // not implemented
+        MyFilter(const MyFilter&); // not implemented
+    };
+
+} // namespace pdal
+
+#endif
